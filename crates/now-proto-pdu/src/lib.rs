b24add86c56c699fe2e4a7be6af8f91a32909156
Vladyslav Nikonov <pacmancoder@gmail.com>
2024-12-13 16:22:37 +0200

    feat(initial): initial now-proto-pdu code from IronRDP (#2)

--- crates/now-proto-pdu/src/lib.rs
+++ crates/now-proto-pdu/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![doc = include_str!("../../../README.md")]
+#![doc(
+    html_logo_url = "https://webdevolutions.blob.core.windows.net/images/projects/devolutions/logos/devolutions-icon-shadow.svg"
+)]
+#![no_std]
+
+extern crate alloc;
+extern crate ironrdp_core;
+
+/// Asserts that constant expressions evaluate to `true`.
+///
+/// From <https://docs.rs/static_assertions/1.1.0/src/static_assertions/const_assert.rs.html#51-57>
+#[macro_export]
+macro_rules! const_assert {
+    ($x:expr $(,)?) => {
+        #[allow(unknown_lints, clippy::eq_op)]
+        const _: [(); 0 - !{
+            const ASSERT: bool = $x;
+            ASSERT
+        } as usize] = [];
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Ensure that we do not compile on platforms with less than 4 bytes per u32. It is pretty safe
+// to assume that NOW-PROTO will not ever be used on 8/16-bit MCUs or CPUs.
+//
+// This is required to safely cast u32 to usize without additional checks.
+const_assert!(size_of::<usize>() >= 4);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[macro_use]
+mod macros;
+
+mod core;
+mod exec;
+mod message;
+mod session;
+mod system;
+
+pub use core::*;
+
+pub use exec::*;
+pub use message::*;
+pub use session::*;
+pub use system::*;
