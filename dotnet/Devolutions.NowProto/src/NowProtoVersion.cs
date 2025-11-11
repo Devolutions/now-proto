47a1b60114c0b8f1a2c2771c7dd10c63dc4b38ae
Vladyslav Nikonov <mail@pacmancoder.xyz>
2025-11-11 17:18:06 +0200

    Fixed current version info in capabilities

--- dotnet/Devolutions.NowProto/src/NowProtoVersion.cs
+++ dotnet/Devolutions.NowProto/src/NowProtoVersion.cs
@@ -2,7 +2,7 @@
 {
     public readonly record struct NowProtoVersion(ushort Major, ushort Minor) : IComparable<NowProtoVersion>
     {
-        public static NowProtoVersion Current => new(1, 3);
+        public static NowProtoVersion Current => new(1, 4);
 
         // -- IComparable<NowProtoVersion> --
         public int CompareTo(NowProtoVersion other)
